d09e2ac758632a54b82393795e7d14b91393da20
Alex Titov <54718346+alextitov19@users.noreply.github.com>
2023-08-27 12:26:44 -0700

    state location

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,5 +1,5 @@
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
-import { useMemo } from "react";
+import { useState } from "react";
 import "./Map.css";
 
 const Map = () => {
@@ -9,7 +9,25 @@ const Map = () => {
         mapIds: ["bee90f6129aa635c"]
     });
 
-    const center = useMemo(() => ({ lat: 37.363577, lng: -120.424730 }), []);
+    const [lat, setLat] = useState(37.363577);
+    const [lng, setLng] = useState(-120.424730);
+
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+        console.log("Geolocation not supported");
+    }
+
+    function success(position) {
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+        console.log(`Latitude: ${lat}, Longitude: ${lng}`);
+    }
+
+    function error() {
+        console.log("Unable to retrieve your location");
+    }
 
     return (
         <div>
@@ -18,7 +36,7 @@ const Map = () => {
             ) : (
                 <GoogleMap
                     mapContainerClassName="map-container"
-                    center={center}
+                    center={{ lat: lat, lng: lng }}
                     zoom={16}
                     options={{ mapId: "bee90f6129aa635c" }}
                 >
