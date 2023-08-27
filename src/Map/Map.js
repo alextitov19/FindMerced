6c7fc4d3e4f02579910c4aa471b155c8db80a3a5
Alex Titov <54718346+alextitov19@users.noreply.github.com>
2023-08-27 11:16:58 -0700

    removed markers

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -6,6 +6,7 @@ const Map = () => {
 
     const { isLoaded } = useLoadScript({
         googleMapsApiKey: "AIzaSyB0pclggmDHDuKDKs4-2N5_eFUYGxAt7KA",
+        mapIds: ["bee90f6129aa635c"]
     });
 
     const center = useMemo(() => ({ lat: 37.363577, lng: -120.424730 }), []);
@@ -19,7 +20,12 @@ const Map = () => {
                     mapContainerClassName="map-container"
                     center={center}
                     zoom={16}
-                />
+                    options={{ mapId: "bee90f6129aa635c" }}
+                >
+                    <Marker position={{ lat: 37.363577, lng: -120.424730 }} />
+                    <Marker position={{ lat: 37.364577, lng: -120.424730 }} />
+
+                </GoogleMap>
             )}
         </div>
     );
